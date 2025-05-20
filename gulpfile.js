01dd7e66ea6a5b4c8b526035dc6d3aee144b2371
Andrés Bulacia <andresbulacia040@gmail.com>
2025-05-20 17:05:02 +0200

    minimización en js y css

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 import {src, dest, watch, series} from 'gulp'; 
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import terser from 'gulp-terser';
 
 const sass = gulpSass(dartSass);
 
 export function js(done) {
     src('src/js/app.js')
+    .pipe(terser())
     .pipe(dest('build/js'));
     done();
 }
 
-export function css(done) {
+export function css( done ) {
     src('src/scss/app.scss', {sourcemaps: true})
-        .pipe( sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError) )
-        .pipe( dest('build/css', {sourcemaps: true}) )
+        .pipe(sass({ outputStyle: 'compressed' })
+            .on('error', sass.logError))
+        .pipe( dest('build/css', {sourcemaps: '.'}) )
 
     done();
 }
